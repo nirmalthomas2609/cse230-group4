b4fd43c0380e2c0845d0a887aec8e2fdb3acf574
Venkat Krishnamohan <vk@Venkats-MacBook-Air.local>
2022-12-07 00:50:52 -0800

    Update remove redundant test

--- test/Test.hs
+++ test/Test.hs
@@ -33,18 +33,19 @@ import Ship ( Direction(West, North, South, East),
 
 main :: IO ()
 main = runTests
-    [ testIsRockEnd,
-      testHasCollided,
+    [ unitTests,
       propTests
     ]
 
-testIsRockEnd ::  Score -> TestTree
-testIsRockEnd sc = testGroup "List" [
+unitTests ::  Score -> TestTree
+unitTests sc = testGroup "List" [
   scoreTest ((\_ -> isRockEnd ((0,4), 1)), (), True, 1, "rockEndCheck1"),
   scoreTest ((\_ -> isRockEnd ((3,6), 1)), (), False, 1, "rockEndCheck2"),
   scoreTest ((\_ -> isRockEnd ((44,4), 0)), (), True, 1, "rockEndCheck3"),
   scoreTest ((\_ -> _score (resetScore (Game {_score = 9}))), (), 0, 1, "resetScore4"),
-  scoreTest ((\_ -> length (killRocks [((45, 3), 0), ((46, 0), 0), ((0, 0), 1)])), (), 0, 1, "killRocks1")
+  scoreTest ((\_ -> length (killRocks [((45, 3), 0), ((46, 0), 0), ((0, 0), 1)])), (), 0, 1, "killRocks1"),
+  scoreTest ((\_ -> hasCollided [(0,4),(-1, 4),(-1, 5)] ((-1, 5), 1)), (), True, 1, "Collided-1"),
+  scoreTest ((\_ -> hasCollided [(0,4),(-1, 4),(-1, 5),(0, 5)] ((0, 6), 1)), (), False, 1, "Collided-2")
     ]
     where
         scoreTest :: (Show b, Eq b) => (a -> b, a, b, Int, String) -> TestTree
@@ -66,32 +67,10 @@ propUpdateScore :: (Int, Int) -> Property
 propUpdateScore (x, y) = 
     (x >= 0 && y >= 0) ==> _score (updateScore x (Game {_score = y})) == x + y
 
--- propKillRocks :: Int -> Property
--- propKillRocks x = 
---     (x == 45) ==> length (killRocks [((x, 3), 0)]) == 0
-
--- propHasCollided :: (Int,Int) -> Property
--- propHasCollided (x, y) = 
---     (x>=0 && y>=0) ==> 
-
 propTests :: Score -> TestTree
 propTests sc = testGroup "Property Based Tests" [ 
     scoreProp sc ("prop_is_rock_end_false", propIsRockEndFalse, 1),
     scoreProp sc ("prop_update_score", propUpdateScore, 1),
     scoreProp sc ("prop_is_rock_end_true", propIsRockEndTrue, 1),
     scoreProp sc ("prop_is_rock_end_true2", propIsRockEndTrue2, 1)
-    ]
-
-testHasCollided :: Score -> TestTree
-testHasCollided sc = testGroup "HasCollided" [
-    scoreTest ((\_ -> hasCollided [(0,4),(-1, 4),(-1, 5)] ((-1, 5), 1)), (), True, 1, "Collided-1"),
-    scoreTest ((\_ -> hasCollided [(0,4),(-1, 4),(-1, 5),(0, 5)] ((0, 6), 1)), (), False, 1, "Collided-2")
-    ]
-    where
-        scoreTest :: (Show b, Eq b) => (a -> b, a, b, Int, String) -> TestTree
-        scoreTest (f, x, r, n, msg) = scoreTest' sc (return . f, x, r, n, msg)
-
-testRockProducer :: Score -> TestTree
-testRockProducer sc = testGroup "RockProducer" [
-    scoreTest ((\_ -> rockProducer), (V2 0 1), ((0, 1), 0), 1, "RP-1")
     ]
