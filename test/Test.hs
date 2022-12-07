53a5c81dcae7739619709ca9a370e8ea4ee12f3e
sumanthcherupally <sumanthcherupally@gmail.com>
2022-12-06 19:49:08 -0800

    Added tests and framework for testing

--- test/Test.hs
+++ test/Test.hs
@@ -3,11 +3,35 @@
 
 import Test.Tasty
 import Common
-import Ship
+import Ship ( Direction(West, North, South, East),
+      Game,
+      Coord,
+      dead,
+      rocks,
+      score,
+      time,
+      ship,
+      endState,
+      isRockEnd,
+      height,
+      width,
+      step,
+      turn,
+      decrementTimer,
+      initGame,
+      initMenu,
+      endGame )
 
 main :: IO ()
 main = runTests
-    [ testIsRockEnd
-    ]
+    [ testIsRockEnd ]
 
 testIsRockEnd ::  Score -> TestTree
+testIsRockEnd sc = testGroup "List" [
+  scoreTest ((\_ -> isRockEnd ((0,4), 1)), (), True, 1, "t-1"),
+  scoreTest ((\_ -> isRockEnd ((3,6), 1)), (), False, 1, "t-2"),
+  scoreTest ((\_ -> isRockEnd ((44,4), 0)), (), True, 1, "t-3")
+    ]
+    where
+        scoreTest :: (Show b, Eq b) => (a -> b, a, b, Int, String) -> TestTree
+        scoreTest (f, x, r, n, msg) = scoreTest' sc (return . f, x, r, n, msg)
